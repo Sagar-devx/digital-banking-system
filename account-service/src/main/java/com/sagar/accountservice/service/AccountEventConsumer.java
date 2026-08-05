0c7ffed1af976fdcf8f53a63c869a8c35f01ee46
Sagar-devx <sagarsharma.devx@gmail.com>
2026-08-05 17:08:14 +0530

    Implement event consumer

--- account-service/src/main/java/com/sagar/accountservice/service/AccountEventConsumer.java
+++ account-service/src/main/java/com/sagar/accountservice/service/AccountEventConsumer.java
@@ -1,4 +1,60 @@
 package com.sagar.accountservice.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class AccountEventConsumer {
-}
+
+    private final AccountService accountService;
+
+    /**
+     * Consume transaction.ompleted event from kafka
+     * credits receiver account
+     * @param payload
+     */
+    @KafkaListener(topics ="transaction.completed")
+    public void consumeTransactionComplete(
+            @Payload Map<String,Object> payload){
+
+        try{
+            String receiverAccount = (String) payload.get("receiverAccountNumber");
+            BigDecimal amount = new BigDecimal(payload.get("account").toString());
+
+            log.info("creating account : {} amount : {}", receiverAccount, amount);
+
+            accountService.creditBalance(receiverAccount, amount);
+        }
+        catch(Exception e){
+            log.error("error while crediting account : {} ", e.getMessage());
+        }
+    }
+
+    /**
+     * conume fraud.detected event from kafka
+     * Blocks the flagged account
+     * @param payload
+     */
+    @KafkaListener(topics = "fraud.detected")
+    public void consumeFraudDetected(
+            @Payload Map<String,Object> payload){
+        try{
+            String accountNumber = (String) payload.get("accountNumber");
+            log.info("Fraud detected - blocking account : {} ", accountNumber);
+
+            accountService.blockAccount(accountNumber);
+        }
+        catch(Exception e){
+            log.error("error while crediting account : {} ", e.getMessage());
+        }
+    }
+
+    }
--- account-service/src/main/java/com/sagar/accountservice/service/AccountService.java
+++ account-service/src/main/java/com/sagar/accountservice/service/AccountService.java
@@ -2,76 +2,20 @@ package com.sagar.accountservice.service;
 
 import com.sagar.accountservice.dto.AccountResponse;
 import com.sagar.accountservice.dto.CreateAccountRequest;
-import com.sagar.accountservice.entity.Account;
-import com.sagar.accountservice.entity.AccountStatus;
-import com.sagar.accountservice.entity.AccountType;
-import com.sagar.accountservice.repository.AccountRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.security.SecureRandom;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class AccountService {
+public interface AccountService {
 
-    private final AccountRepository accountRepository;
-    private static final SecureRandom secureRandom = new SecureRandom();
+    AccountResponse createAccount(CreateAccountRequest request);
 
-    public AccountResponse createAccount(CreateAccountRequest request) {
-        log.info("Create account for : {}", request.getEmail());
+    AccountResponse getAccount(String accountNumber);
 
-        if (accountRepository.existsByEmail(request.getEmail())) {
-            throw new RuntimeException("Account already exists");
-        }
-        Account account = new Account();
-        account.setEmail(request.getEmail());
-        account.setPhone(request.getPhone());
-        account.setAccountHolderName(request.getAccountHolderName());
-        account.setAccountType(request.getAccountType());
-        account.setBalance(request.getInitialDeposit());
-        account.setStatus(AccountStatus.ACTIVE);
-        account.setAccountNumber(generateAccountNumber());
-        account.setDailyTransactionLimit(
-                request.getAccountType() == AccountType.SAVINGS
-                        ? new BigDecimal("100000")
-                        : new  BigDecimal("500000")
-        );
+    BigDecimal getBalance(String accountNumber);
 
-        Account savedAccount = accountRepository.save(account);
+    void blockAccount(String accountNumber);
 
-        log.info("Account created : {}", savedAccount.getAccountNumber());
+    void deductBalance(String accountNumber, BigDecimal amount);
 
-        return mapToResponse(savedAccount);
-    }
-
-    private String generateAccountNumber() {
-
-        String accountNumber;
-
-        do{
-            long number =  secureRandom.nextLong(1000000000000L);
-            accountNumber = String.format("%012d", number);
-
-        }while (accountRepository.existsByAccountNumber(accountNumber));
-        return accountNumber;
-    }
-
-    private AccountResponse mapToResponse(Account account) {
-        AccountResponse accountResponse = new AccountResponse();
-        accountResponse.setAccountNumber(account.getAccountNumber());
-        accountResponse.setBalance(account.getBalance());
-        accountResponse.setDailyTransactionLimit(account.getDailyTransactionLimit());
-        accountResponse.setId(account.getId());
-        accountResponse.setAccountHolderName(account.getAccountHolderName());
-        accountResponse.setAccountType(account.getAccountType());
-        accountResponse.setEmail(account.getEmail());
-        accountResponse.setPhone(account.getPhone());
-        accountResponse.setStatus(account.getStatus());
-        accountResponse.setCreatedAt(account.getCreatedAt());
-        return accountResponse;
-    }
+    void creditBalance(String accountNumber, BigDecimal amount);
 }
